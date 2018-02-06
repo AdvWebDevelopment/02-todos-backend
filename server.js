c81974df22988d809bab6fda905b9198acc1c001
kanyaruk <ahmkanyaruk@hotmail.com>
2018-02-07 00:10:30 +0700

    issue 1 post, put, delete

--- server.js
+++ server.js
@@ -7,13 +7,80 @@ const mongoose = require('mongoose')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`, { useMongoClient: true })
+mongoose.connect(`mongodb://${process.env.MONGO_HOST}/todo`, { useMongoClient: true })
 const port = process.env.PORT || 9999
 const Todo = require('./app/models/todo')
+
+// let members = [
+//   {'name': 'ton'},
+//   {'name': 'ahm'}
+// ]
+
+// app.get('/member', (request, response) => {
+//   response.json(members)
+// })
+
+// app.get('/member/:id', (request, response) => {
+//   const id = request.param('id')
+//   response.json(members[id])
+// })
+
+// app.post('/test', (request, response) => {
+//   console.log(request.body)
+//   response.json('Hello Post Method')
+// })
 
 app.get('/todos', (request, response) => {
   Todo.find().then(todos => {
     response.json({todos})
+  })
+})
+
+app.post('/todos', (request, response) => {
+  const body = request.body
+  const todo = new Todo(body)
+  todo.save((err, todo) => {
+    if (err) {
+      response.json(err)
+    } else {
+      response.json(todo)
+    }
+  })
+})
+
+app.put('/todos/:id', (request, response) => {
+  const body = request.body
+  const id = request.params.id
+  Todo.findById(id, function (err, todo) {
+    if (err) {
+      response.json(err)
+    } else {
+      todo.set(body)
+      todo.save(function (err, updatedTodo) {
+        if (err) {
+          response.json(err)
+        } else {
+          response.json(updatedTodo)
+        }
+      })
+    }
+  })
+})
+
+app.delete('/todos/:id', (request, response) => {
+  const id = request.params.id
+  Todo.findById(id, function (err, todo) {
+    if (err) {
+      response.json(err)
+    } else {
+      todo.remove((err, removedTodo) => {
+        if (err) {
+          response.json(err)
+        } else {
+          response.json(removedTodo)
+        }
+      })
+    }
   })
 })
 
