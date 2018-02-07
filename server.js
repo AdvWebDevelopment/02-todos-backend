180d28d6451f6fa42aaf6838f46a4c9bdd209bab
nuttakit2405 <5806021631033@fitm.kmutnb.ac.th>
2018-02-07 19:07:40 +0700

    new I

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect(`mongodb://${process.env.MONGO_HOST}/todo`, { useMongoClient: true })
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/Todo`, { useMongoClient: true })
 const port = process.env.PORT || 9999
 const Todo = require('./app/models/todo')
 
