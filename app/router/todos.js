cd6ffb23c83e7c87a7adf6f2dfa591a1afe2853b
kanyaruk <ahmkanyaruk@hotmail.com>
2018-02-09 22:45:20 +0700

    challenge 2

---
+++ app/router/todos.js
@@ -0,0 +1,61 @@
+var express = require('express')
+var router = express.Router()
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://${process.env.MONGO_HOST}/todo`, { useMongoClient: true })
+const Todo = require('../models/todo.js')
+
+router.get('/todos', (request, response) => {
+  Todo.find().then(todos => {
+    response.json({todos})
+  })
+})
+
+router.post('/todos', (request, response) => {
+  const body = request.body
+  const todo = new Todo(body)
+  todo.save((err, todo) => {
+    if (err) {
+      response.json(err)
+    } else {
+      response.json(todo)
+    }
+  })
+})
+
+router.put('/todos/:id', (request, response) => {
+  const body = request.body
+  const id = request.params.id
+  Todo.findById(id, (err, todo) => {
+    if (err) {
+      response.json(err)
+    } else {
+      todo.set(body)
+      todo.save((err, updatedTodo) => {
+        if (err) {
+          response.json(err)
+        } else {
+          response.json(updatedTodo)
+        }
+      })
+    }
+  })
+})
+
+router.delete('/todos/:id', (request, response) => {
+  const id = request.params.id
+  Todo.findById(id, (err, todo) => {
+    if (err) {
+      response.json(err)
+    } else {
+      todo.remove((err, removedTodo) => {
+        if (err) {
+          response.json(err)
+        } else {
+          response.json(removedTodo)
+        }
+      })
+    }
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,87 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const todos = require('./app/router/todos.js')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect(`mongodb://${process.env.MONGO_HOST}/todo`, { useMongoClient: true })
+app.use('/', todos)
+
 const port = process.env.PORT || 9999
-const Todo = require('./app/models/todo')
-
-// let members = [
-//   {'name': 'ton'},
-//   {'name': 'ahm'}
-// ]
-
-// app.get('/member', (request, response) => {
-//   response.json(members)
-// })
-
-// app.get('/member/:id', (request, response) => {
-//   const id = request.param('id')
-//   response.json(members[id])
-// })
-
-// app.post('/test', (request, response) => {
-//   console.log(request.body)
-//   response.json('Hello Post Method')
-// })
-
-app.get('/todos', (request, response) => {
-  Todo.find().then(todos => {
-    response.json({todos})
-  })
-})
-
-app.post('/todos', (request, response) => {
-  const body = request.body
-  const todo = new Todo(body)
-  todo.save((err, todo) => {
-    if (err) {
-      response.json(err)
-    } else {
-      response.json(todo)
-    }
-  })
-})
-
-app.put('/todos/:id', (request, response) => {
-  const body = request.body
-  const id = request.params.id
-  Todo.findById(id, (err, todo) => {
-    if (err) {
-      response.json(err)
-    } else {
-      todo.set(body)
-      todo.save((err, updatedTodo) => {
-        if (err) {
-          response.json(err)
-        } else {
-          response.json(updatedTodo)
-        }
-      })
-    }
-  })
-})
-
-app.delete('/todos/:id', (request, response) => {
-  const id = request.params.id
-  Todo.findById(id, (err, todo) => {
-    if (err) {
-      response.json(err)
-    } else {
-      todo.remove((err, removedTodo) => {
-        if (err) {
-          response.json(err)
-        } else {
-          response.json(removedTodo)
-        }
-      })
-    }
-  })
-})
-
 app.listen(port)
 console.log('Server is running on port: ' + port)
