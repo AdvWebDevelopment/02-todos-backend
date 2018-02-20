bab9ab1b2b9d234370ac5dcf8f21b534cd45ff53
Kanyaruk <ahmkanyaruk@hotmail.com>
2018-02-20 23:19:32 +0700

    เปลี่ยน var เป็น const

--- app/router/todos.js
+++ app/router/todos.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 const mongoose = require('mongoose')
 mongoose.connect(`mongodb://${process.env.MONGO_HOST}/todo`, { useMongoClient: true })
 const Todo = require('../models/todo.js')
