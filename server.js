2d49b08d8905e6e4d24310a8cac0bb748b9e0e82
kanyaruk <ahmkanyaruk@hotmail.com>
2018-02-26 22:32:58 +0700

    add cors

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-var jwt = require('express-jwt')
+const jwt = require('express-jwt')
+const cors = require('cors')
 
 const todos = require('./app/router/todos.js')
 const user = require('./app/router/user.js')
 const login = require('./app/router/login.js')
 
 app.use(jwt({ secret: process.env.JWT_SECRET }).unless({path: ['/login', '/signup']}))
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/todos', todos)
 app.use('/', user)
+app.use('/todos', todos)
 app.use('/login', login)
 
 const port = process.env.PORT || 9999
